62d9dd181bb4e5452d508561c665d05a91092158
Melih Can Demir <melihcandemir@protonmail.com>
2025-08-21 18:23:13 +0300

    question-store deleted

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -139,7 +139,7 @@ function QuizPageContent() {
             
             return {
               ...subject,
-              questionCount: questionCount
+              questionCount
             };
           });
           
